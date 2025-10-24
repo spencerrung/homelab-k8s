2a491cd61e646d4defa10d701f04cf3b505f91e9
spencerrung <blufps@gmail.com>
2025-10-24 17:04:01 -0400

    oidc things again

--- infrastructure/base/terraform-configs/authentik/main.tf
+++ infrastructure/base/terraform-configs/authentik/main.tf
@@ -36,40 +36,41 @@ data "authentik_certificate_key_pair" "default" {
   name = "authentik Self-signed Certificate"
 }
 
-# Create OAuth2 Provider for GitLab
-resource "authentik_provider_oauth2" "gitlab" {
-  name               = "GitLab"
-  client_id          = "gitlab"
-  client_type        = "confidential"
-  authorization_flow = data.authentik_flow.default_authorization_flow.id
-  signing_key        = data.authentik_certificate_key_pair.default.id
+# GitLab OAuth2 Provider and Application are now managed by Authentik Blueprint
+# See: infrastructure/base/authentik/gitlab-oidc-blueprint.yaml
+#
+# Terraform cannot properly manage property_mappings (scope mappings) due to
+# provider limitations, so we use Authentik's native blueprint system instead.
+#
+# Commented out to prevent conflicts with blueprint management:
 
-  redirect_uris = [
-    "https://code.alucard.dev/users/auth/openid_connect/callback"
-  ]
+# resource "authentik_provider_oauth2" "gitlab" {
+#   name               = "GitLab"
+#   client_id          = "gitlab"
+#   client_type        = "confidential"
+#   authorization_flow = data.authentik_flow.default_authorization_flow.id
+#   signing_key        = data.authentik_certificate_key_pair.default.id
+#
+#   redirect_uris = [
+#     "https://code.alucard.dev/users/auth/openid_connect/callback"
+#   ]
+# }
 
-  # Note: property_mappings (scope mappings) must be configured manually in Authentik UI
-  # Terraform cannot manage them due to provider limitations with data sources
-  # By omitting this field entirely, Terraform will ignore drift in this attribute
-}
+# resource "authentik_application" "gitlab" {
+#   name              = "GitLab"
+#   slug              = "gitlab"
+#   protocol_provider = authentik_provider_oauth2.gitlab.id
+#   meta_launch_url   = "https://code.alucard.dev"
+#   meta_description  = "GitLab CE - Git repository and CI/CD platform"
+# }
 
-# Create Application
-resource "authentik_application" "gitlab" {
-  name              = "GitLab"
-  slug              = "gitlab"
-  protocol_provider = authentik_provider_oauth2.gitlab.id
-  meta_launch_url   = "https://code.alucard.dev"
-  meta_description  = "GitLab CE - Git repository and CI/CD platform"
-}
+# output "gitlab_client_id" {
+#   value       = authentik_provider_oauth2.gitlab.client_id
+#   description = "GitLab OAuth2 Client ID"
+# }
 
-# Output the client secret (will be stored in Terraform state)
-output "gitlab_client_id" {
-  value       = authentik_provider_oauth2.gitlab.client_id
-  description = "GitLab OAuth2 Client ID"
-}
-
-output "gitlab_client_secret" {
-  value       = authentik_provider_oauth2.gitlab.client_secret
-  description = "GitLab OAuth2 Client Secret"
-  sensitive   = true
-}
+# output "gitlab_client_secret" {
+#   value       = authentik_provider_oauth2.gitlab.client_secret
+#   description = "GitLab OAuth2 Client Secret"
+#   sensitive   = true
+# }
